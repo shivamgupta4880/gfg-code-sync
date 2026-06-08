ec7702ce5129fa4712fe8d934c300e36e0e872a0
Shivam Gupta <118576628+shivamgupta4880@users.noreply.github.com>
2026-06-08 11:46:05 +0530

    Solve: Start Coding (Java)

---
+++ GeeksForGeeks/gfg/Start Coding/solution.java
@@ -0,0 +1,24 @@
+/*
+Problem:
+Start Coding
+
+Link:
+https://www.geeksforgeeks.org/problems/start-coding-1/1
+
+Difficulty:
+Unknown
+
+Language:
+Java
+
+Author:
+shivamgupta4880
+
+*/
+
+class GFG {
+    public static void main(String[] args) {
+        // code here
+        System.out.println("Hello World");
+    }
+}
