17584de0af24217ff4efe7cb8dff9f66b9b6ac7a
Shivam Gupta <118576628+shivamgupta4880@users.noreply.github.com>
2026-07-01 13:37:55 +0530

    Solve: Sort Array (Java)

---
+++ GeeksForGeeks/gfg/Sort Array/solution.java
@@ -0,0 +1,24 @@
+/*
+Problem:
+Sort Array
+
+Link:
+https://www.geeksforgeeks.org/problems/sort-the-array0055/1
+
+Difficulty:
+Unknown
+
+Language:
+Java
+
+Author:
+shivamgupta4880
+
+*/
+
+class Solution {
+    void sortArr(int[] arr) {
+        // code here
+        Arrays.sort(arr);
+    }
+}
