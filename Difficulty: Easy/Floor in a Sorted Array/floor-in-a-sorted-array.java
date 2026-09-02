bd3e548b794b9c272fa8c67c80e7358e0535adcc
Shivam Gupta <118576628+shivamgupta4880@users.noreply.github.com>
2026-09-03 01:02:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Floor in a Sorted Array/floor-in-a-sorted-array.java
@@ -0,0 +1,12 @@
+class Solution {
+    static int findFloor(int[] arr, int x) {
+        // code here
+        int max=-1;
+        for(int i=0;i<arr.length;i++){
+            if(x>=arr[i]){
+                max=i;
+            }
+        }
+        return max;
+    }
+}
